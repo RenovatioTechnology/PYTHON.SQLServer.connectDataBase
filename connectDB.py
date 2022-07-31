1e11d8fb802fbeb69d7610321fef23b3c00a873e
mysql-python-cnnet <Renovatio3d@gmail.com>
2022-07-31 17:31:09 -0400

    Initial commit

--- connectDB.py
+++ connectDB.py
@@ -4,19 +4,21 @@ mydb = mysql.connector.connect(
     # url for database
     host='localhost',
     user='root',
-    passwd='pwd1235',
-    database = 'testdb',
+    passwd='pwd1235!',
+    database='testdb',
 )
 my_cursor = mydb.cursor()
 # execute command, create database
-my_cursor.execute('CREATE DATABASE testdb')
-# Check if database exist, show them all in the cursor
-my_cursor.execute('SHOW DATABASES')
-for db in my_cursor:
-    print(db[0])
-# Create a table
-my_cursor.execute('CREATE TABLE user()')
+# my_cursor.execute('CREATE DATABASE testdb')
 
+# Check if database exist, show them all in the cursor #
+# my_cursor.execute('SHOW DATABASES')
+# for db in my_cursor:
+# print(db[0])
 
-
-
+# Create a table, with columns #
+# my_cursor.execute('CREATE TABLE user(name VARCHAR(255), email VARCHAR(255), age INTEGER(10), user_id INTEGER AUTO_INCREMENT PRIMARY KEY)')
+# test
+my_cursor.execute('SHOW TABLES')
+for table in my_cursor:
+    print(table)
