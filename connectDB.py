43daa11a62f86b657aeb73f6c63dc902a27d78a5
mysql-python-cnnet <Renovatio3d@gmail.com>
2022-07-31 15:20:54 -0400

    Initial commit

---
+++ connectDB.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    # url for database
+    host='localhost',
+    user='root',
+    passwd='pwd1235',
+    database = 'testdb',
+)
+my_cursor = mydb.cursor()
+# execute command, create database
+my_cursor.execute('CREATE DATABASE testdb')
+# Check if database exist, show them all in the cursor
+my_cursor.execute('SHOW DATABASES')
+for db in my_cursor:
+    print(db[0])
+# Create a table
+my_cursor.execute('CREATE TABLE')
---
+++ if.py
@@ -0,0 +1,14 @@
+var1 = 100
+if var1:
+    print("1 - Got a true expression value")
+
+print(var1)
+var2 = 'hello'
+if var2:
+    print("2 - Got a true expression value")
+print(var2)
+num=input("enter a value")
+if num>100:
+    print("its more than 100")
+else:
+    print("its not more than 100")
