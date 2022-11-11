d35953c8141836d5cd55fd2a93fdcc81e9975e35
mysql-python-cnnet <Renovatio3d@gmail.com>
2022-11-11 14:25:07 -0500

    Fetch using LIKE

--- connectDB.py
+++ connectDB.py
@@ -40,7 +40,7 @@ my_cursor = mydb.cursor()
 # mydb.commit()
 
 # Pulling data from database #
-# my_cursor.execute('SELECT * FROM user')
+# my_cursor.execute('SELECT * FROM users')
 # fetchAll = my_cursor.fetchall()
 # Header
 # print('NAME\t\tEMAIL\t\tAGE\tID')
@@ -51,49 +51,53 @@ my_cursor = mydb.cursor()
 #   print(row[0] + '\t %s' % row[1] + '\t%s' % row[2] + '\t%s' % row[3])
 
 # WHERE Clause, finding all specific search
-# my_cursor.execute("SELECT * FROM user WHERE name = 'Jeff'")
-# my_cursor.execute('SELECT * FROM user WHERE age > 30')
+# my_cursor.execute("SELECT * FROM users WHERE name = 'Jeff'")
+# my_cursor.execute('SELECT * FROM users WHERE age > 30')
 # fetchAll = my_cursor.fetchall()
 # for row in fetchAll:
 #  print(row)
 
 
 # LIKE Clause, finding all specific search
-# my_cursor.execute("SELECT * FROM user WHERE name LIKE '%i%'")
+# my_cursor.execute("SELECT * FROM users WHERE name LIKE '%i%'")
 # using AND return name with 'e' and age, OR only one has to be true
-# my_cursor.execute("SELECT * FROM user WHERE name LIKE '%e%' OR age = 39")
-# my_cursor.execute("SELECT * FROM user WHERE name LIKE '%e%' AND age = 39")
-# my_cursor.execute("SELECT * FROM user WHERE name LIKE 'J%'")
-# my_cursor.execute('SELECT * FROM user WHERE age > 30')
+# my_cursor.execute("SELECT * FROM users WHERE name LIKE '%e%' OR age = 39")
+# my_cursor.execute("SELECT * FROM users WHERE name LIKE '%e%' AND age = 39")
+# my_cursor.execute("SELECT * FROM users WHERE name LIKE 'J%'")
+# my_cursor.execute('SELECT * FROM users WHERE age > 30')
 # fetchAll = my_cursor.fetchall()
 # for row in fetchAll:
 #    print(row)
 
 # Updating Record
-# my_sql = "UPDATE user SET name = 'Jefferson' WHERE user_id = 10"
-# my_sql = "UPDATE user SET age = 13 WHERE user_id = 10"
-# my_sql = "UPDATE user SET age = 39 WHERE name = 'Jeff'"
+# my_sql = "UPDATE users SET name = 'Jefferson' WHERE user_id = 10"
+# my_sql = "UPDATE users SET age = 13 WHERE user_id = 10"
+# my_sql = "UPDATE users SET age = 39 WHERE name = 'Jeff'"
 # Execute
 # my_cursor.execute(my_sql)
 # Commit the change to mysql, to save it
 # mydb.commit()
 
 # Limiting and Ordering result
-# my_cursor.execute("SELECT * FROM user LIMIT 3")
-# my_cursor.execute("SELECT * FROM user LIMIT 3 OFFSET 1")
+# my_cursor.execute("SELECT * FROM users LIMIT 3")
+# my_cursor.execute("SELECT * FROM users LIMIT 3 OFFSET 1")
 # Arrange Descendants
-# my_cursor.execute("SELECT * FROM user ORDER BY name DESC")
+# my_cursor.execute("SELECT * FROM users ORDER BY name DESC")
 # Arrange Ascendants
-# my_cursor.execute("SELECT * FROM user ORDER BY name ASC")
+# my_cursor.execute("SELECT * FROM users ORDER BY name ASC")
 # Arrange by Age
-# my_cursor.execute("SELECT * FROM user ORDER BY age ASC")
-# my_cursor.execute("SELECT * FROM user ORDER BY age DESC")
+# my_cursor.execute("SELECT * FROM users ORDER BY age ASC")
+# my_cursor.execute("SELECT * FROM users ORDER BY age DESC")
 # fetchAll = my_cursor.fetchall()
 # for row in fetchAll:
 #    print(row)
 
 # Deleting
-my_sql = "DELETE FROM user WHERE user_id = 10"
-my_cursor.execute(my_sql)
+# my_sql = "DELETE FROM users WHERE user_id = 10"
+# my_cursor.execute(my_sql)
 # Commit the change to mysql, to save it
-mydb.commit()
+# mydb.commit()
+
+# Delete tables, backup tables,export tables
+my_sql = "DROP TABLE IF EXISTS user"
+my_cursor.execute(my_sql)
